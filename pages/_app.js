bbdb803396925a5229d4ef54797864de2477ae38
tradergalax <429224280@qq.com>
2023-08-05 15:56:22 +0400

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,23 @@
 import "@/styles/globals.css";
-import { nanoid } from "nanoid";
 import Layout from "../components/Layout";
 
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
+import { optimism, zora, base } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, zora],
-  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+  [optimism, zora, base],
+  [
+    alchemyProvider({ apiKey: "LQ0xqhSEYELkJL2ToAS0S02mh8LiT_iR" }),
+    publicProvider(),
+  ]
 );
 const { connectors } = getDefaultWallets({
-  appName: "My RainbowKit App",
-  projectId: nanoid(),
+  appName: "App",
+  projectId: "2a612b9a18e81ce3fda2f82787eb6a4a",
   chains,
 });
 
